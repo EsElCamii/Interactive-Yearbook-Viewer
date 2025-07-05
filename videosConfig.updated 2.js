4e69466c6446a10824712e733f37c3a9158912ad
Camilo Quirós Mendoza <camii.twitchtv@gmail.com>
2025-07-04 20:37:05 -0600

    Fix video loading and improve media overlay

---
+++ videosConfig.updated 2.js
@@ -0,0 +1,9 @@
+// Updated on 2025-07-03T18:02:05.057Z
+module.exports = {
+  "38": {
+    "src": "https://res.cloudinary.com/dh4em7ejt/video/upload/v1751565684/yearbook-videos/diaMuertos.mp4",
+    "translateX": "15.5vmin",
+    "translateY": "51vmin",
+    "width": "26vmin"
+  }
+};
